446a6ea8e0372d84eb564021a7e2c84f71f231f4
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2019-11-12 11:51:13 +0930

    python version

--- python_scripts/rotunno.py
+++ python_scripts/rotunno.py
@@ -1,15 +1,106 @@
-# Third party libraries
+# System
+import datetime
+
+# Performance
+from numba import jit
+
+# Plotting
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib.animation import FuncAnimation
+
+# Analysis
 import numpy as np
-import datetime
+import xarray as xr
+
+def solve_rotunno_case_two(xin=161,zetaN=81,tauN=32,kN=2001,
+                           xi0=0.2, latitude=5, h=1500,
+                           delTheta=6, theta0=300, theta1=350,
+                           tropHeight=12000):
+
+    # Time interval
+    delTau = 2*np.pi/tauN
+
+    # Initialise solution arrays
+    psi = np.zeros((xiN,zetaN,tauN))
+    u = np.zeros((xiN,zetaN,tauN))
+    v = np.zeros((xiN,zetaN,tauN))
+    w = np.zeros((xiN,zetaN,tauN))
+    b = np.zeros((xiN,zetaN,tauN))
+
+    # Initialise domains
+    xi = np.linspace(-2,2,xiN)
+    zeta = np.linspace(0,4,zetaN)
+    tau = np.arange(0,2*np.pi,delTau)
+    k = np.linspace(0,20,kN)
+
+    # Define constants
+    omega = 7.2921159 * 10.0 ** (-5)
+    g = 9.80665
+
+    # Calculate nondimensional model parameters from inputs
+    f = 2.0*omega*np.sin(latitude * np.pi / 180.0)
+    N = np.sqrt((g/theta0)*(theta1-theta0)/tropHeight) # Brunt Vaisala Frequency
+    beta = omega**2/(N*np.sqrt(omega**2-f**2))
+    Atilde = .5*delTheta*(g/(np.pi*theta0))*h**(-1)*omega**(-3)/(12*60*60)
+
+    psi, u, w = integrate_case_two(xi,zeta,tau,k,xi0,beta,Atilde)
+
+    ds = xr.Dataset({'psi':(('tau','zeta','xi'),psi),
+                     'u':(('tau','zeta','xi'),u),
+                     'w':(('tau','zeta','xi'),w)},
+                    {'tau': tau, 'zeta':zeta, 'xi':xi},
+                    {'xi0':xi0, 'latitude':latitude, 'h':h,
+                     'delTheta':delTheta, 'theta0':theta0,
+                     'theta1':theta1, 'tropHeight':tropHeight})
+
+    now = str(datetime.datetime.now())[0:-7]
+    now=now.replace('-', '').replace(':', '').replace(' ', '_')
+    ds.to_netcdf('../datasets/rotunno_case_2_{}.nc'.format(now))
+
+@jit()
+def integrate_case_two(xi,zeta,tau,k,xi0,beta,Atilde):
+    psi = np.zeros((tau.size, zeta.size, xi.size))
+    u = np.zeros((tau.size, zeta.size, xi.size))
+    w = np.zeros((tau.size, zeta.size, xi.size))
+    psi_integrand = np.zeros(k.size)
+    u_integrand = np.zeros(k.size)
+    w_integrand = np.zeros(k.size)
+    # Perform numerical integration
+    for i in range(xi.size):
+        for j in range(zeta.size):
+            for l in range(tau.size):
+                for m in range(k.size):
+                    psi_integrand[m] = (np.cos(k[m]*xi[i])
+                                        *np.exp(-xi0*k[m])
+                                        *(np.sin(k[m]*zeta[j]+tau[l])
+                                          -np.exp(-zeta[j])*np.sin(tau[l]))
+                                        /(1+k[m]**2))
+                    u_integrand[m] = (k[m]*np.sin(k[m]*xi[i])
+                                      *np.exp(-xi0*k[m])
+                                      *(np.sin(k[m]*zeta[j]+tau[l])
+                                        -np.exp(-zeta[j])*np.sin(tau[l]))
+                                      /(1+k[m]**2))
+                    w_integrand[m] = (np.cos(k[m]*xi[i])
+                                      *np.exp(-xi0*k[m])
+                                      *(k[m]*np.cos(k[m]*zeta[j]+tau[l])
+                                        +np.exp(-zeta[j])*np.sin(tau[l]))
+                                      /(1+k[m]**2))
+
+                psi[l,j,i] = np.trapz(k,psi_integrand)
+                u[l,j,i] = np.trapz(k,u_integrand)
+                w[l,j,i] = np.trapz(k,w_integrand)
+    # Scale
+    psi = -beta * Atilde * psi
+    u = -beta * Atilde * u
+    w = -beta * Atilde * w
+    return psi, u, w
 
 def redimensionalise(ds, h, f, N):
-    
+
     # Specify constants
     omega = 7.2921159 * 10.0 ** (-5)
-    
+
     ds = ds.assign_coords(zeta = ds.zeta * h)
     ds.zeta.attrs['units'] = 'm'
     ds = ds.assign_coords(tau = ds.tau / omega)
@@ -22,18 +113,18 @@ def redimensionalise(ds, h, f, N):
     ds.w.attrs['units'] = 'm/s'
     ds['psi'] = ds.psi * h**2 * omega
     ds.psi.attrs['units'] = 'm^2/s'
-    
+
     if ds.lat < 30:
         ds = ds.assign_coords(xi = ds.xi * N * h * ((omega**2 - f**2)**(-1/2)))
-    elif ds.lat > 30: 
+    elif ds.lat > 30:
         ds = ds.assign_coords(xi = ds.xi * N * h * ((f**2-omega**2)**(-1/2)))
-        
+
     ds.xi.attrs['units'] = 'm'
 
     return ds
 
 def calcTheta(ds):
-    
+
     w = ds['w'].values.T
     xi = ds['xi'].values
     zeta = ds['zeta'].values
@@ -44,66 +135,66 @@ def calcTheta(ds):
     theta1 = ds.theta1
     tropHeight = ds.tropHeight
     N = ds.N
-    
+
     # Specify constants
     omega = 7.2921159 * 10.0 ** (-5)
     g = 9.80665
-    
+
     thetaBar = np.zeros(np.shape(w))
     d_thetaBar_d_z = (theta1-theta0)/tropHeight
     d_thetaBar_d_zeta = h * d_thetaBar_d_z # Recall zeta * h = z
-    
+
     for i in np.arange(0,np.size(tau)):
         thetaBar[:,:,i] = np.outer(
                 np.ones(np.size(xi)), zeta * d_thetaBar_d_zeta
                 )
-    
+
     # Specify heating function array
     Q = np.zeros(np.shape(w))
-    
+
     for i in np.arange(0,np.size(tau)):
         Q[:,:,i] = np.outer(
-            ds.Atilde * ((np.pi / 2) + np.arctan(xi / xi0)), 
-            np.exp(-zeta) 
+            ds.Atilde * ((np.pi / 2) + np.arctan(xi / xi0)),
+            np.exp(-zeta)
             )
         Q[:,:,i] = Q[:,:,i] * np.sin(tau[i])
-        
+
     LHS = np.zeros((np.size(tau),np.size(tau)))
     RHS = np.zeros(np.size(tau))
     dtau = tau[1]-tau[0]
-    
+
     LHS[0, 0] = 1
     if (np.mod(np.size(tau),2) == 0):
         LHS[0,int(np.size(tau)/2)] = 1
     else:
         LHS[0,np.floor(np.size(tau)/2)] = 1/2
         LHS[0,np.floor(np.size(tau)/2)] = 1/2
-    
+
     RHS[0] = 0
     for k in np.arange(1,np.size(tau)):
         LHS[k, np.mod(k+1,32)] = 1
         LHS[k, k] = -1
-    
+
     # Initialise bouyancy matrix
-    btilde = np.zeros(np.shape(w)) 
-    
+    btilde = np.zeros(np.shape(w))
+
     # Calculate bouyancy
     for i in np.arange(0,np.size(xi)):
         for j in np.arange(0, np.size(zeta)):
             for k in np.arange(1,np.size(tau)):
                 RHS[k] = dtau * (Q[i,j,k] - ((N/omega) ** 2) * w[i,j,k])
-                
+
             btilde[i,j,:] = np.linalg.solve(LHS,RHS)
-            
+
     # Convert btilde to potential temperature perturbation
     thetaPrime = btilde * (omega ** 2) * h * theta0 / g
-            
+
     theta = theta0 + thetaBar + thetaPrime
-    
+
     ds = ds.assign(theta=(('xi','zeta','tau'),theta))
     ds = ds.assign(thetaBar=(('xi','zeta','tau'),thetaBar))
     ds = ds.assign(thetaPrime=(('xi','zeta','tau'),thetaPrime))
-    
+
     return ds
 
 def calc_v(ds):
@@ -112,49 +203,49 @@ def calc_v(ds):
     zeta = ds['zeta'].values
     tau = ds['tau'].values
     lat=ds.lat
-    
+
     # Specify constants
     omega = 7.2921159 * 10.0 ** (-5)
-    f = 2 * omega * np.sin(np.deg2rad(lat))   
-    
+    f = 2 * omega * np.sin(np.deg2rad(lat))
+
     LHS = np.zeros((np.size(tau),np.size(tau)))
     RHS = np.zeros(np.size(tau))
     dtau = tau[1]-tau[0]
-    
+
     LHS[0, 0] = 1
     if (np.mod(np.size(tau),2) == 0):
         LHS[0,int(np.size(tau)/2)] = 1
     else:
         LHS[0,np.floor(np.size(tau)/2)] = 1/2
         LHS[0,np.floor(np.size(tau)/2)] = 1/2
-        
+
     RHS[0] = 0
     for k in np.arange(1,np.size(tau)):
         LHS[k, np.mod(k+1,np.size(tau))] = 1
         LHS[k, k] = -1
-        
+
     # Initialise v matrix
-    v = np.zeros(np.shape(w)) 
-    
+    v = np.zeros(np.shape(w))
+
     # Calculate bouyancy
     for i in np.arange(0,np.size(xi)):
         for j in np.arange(0, np.size(zeta)):
             for k in np.arange(1,np.size(tau)):
                 RHS[k] = - dtau * (f/omega) * w[i,j,k]
-                
+
             v[i,j,:] = np.linalg.solve(LHS,RHS)
-            
+
     ds = ds.assign(v=(('xi','zeta','tau'),v))
-            
+
     return ds
-    
+
 
 def plotPsi(ds,t=0):
 
     psi = ds['psi'].values.T
     xi = ds['xi'].values
     zeta = ds['zeta'].values
-    
+
     # Plot
     plt.rc('text', usetex=False)
 
@@ -202,7 +293,7 @@ def plotVelocity(ds, t=0):
     w = ds['w'].values.T
     xi = ds['xi'].values
     zeta = ds['zeta'].values
-        
+
     plt.rc('text', usetex=False)
 
     # Initialise fonts
@@ -227,7 +318,7 @@ def plotVelocity(ds, t=0):
 
     contourPlot=ax.contourf(Xi,Zeta,speed[:,:,t],levels,vmin=speedMin,
                              vmax=speedMax, cmap='Reds')
-    
+
     skip=int(np.floor(np.size(xi)/12))
 
     uQ=np.sign(u)*(np.abs(u)/speedMax)**(2/3)*speedMax
@@ -265,7 +356,7 @@ def animatePsi(ds):
     xi = ds['xi'].values
     zeta = ds['zeta'].values
     tau = ds['tau'].values
-    
+
     # Plot
     plt.rc('text', usetex=False)
 
@@ -290,7 +381,7 @@ def animatePsi(ds):
 
     contourPlot=ax.contourf(Xi,Zeta,psi[:,:,0],levels,vmin=-psiMax,
                              vmax=psiMax, cmap='RdBu_r')
-    
+
     plt.title('Stream function [' + ds.psi.attrs['units'] + ']')
     plt.xlabel('Distance [' + ds.xi.attrs['units'] + ']')
     plt.ylabel('Height [' + ds.zeta.attrs['units'] + ']')
@@ -329,7 +420,7 @@ def animateVelocity(ds):
     xi = ds['xi'].values
     zeta = ds['zeta'].values
     tau = ds['tau'].values
-    
+
     # Plot
     plt.rc('text', usetex=False)
 
@@ -346,8 +437,8 @@ def animateVelocity(ds):
     # Velocity plot
     fig, ax = plt.subplots()
 
-    speed=(u**2+w**2)**(1/2)    
-    speedInc = np.round(np.ceil(np.max(speed)*10)/100,1)    
+    speed=(u**2+w**2)**(1/2)
+    speedInc = np.round(np.ceil(np.max(speed)*10)/100,1)
     speedMax = np.ceil(np.max(speed)*10)/10
     speedMin=0
     levels=np.arange(speedMin,speedMax+speedInc,speedInc)
@@ -384,7 +475,7 @@ def animateVelocity(ds):
         # Update the line and the axes (with a new xlabel). Return a tuple of
         # "artists" that have to be redrawn for this frame.
         ax.collections = []
-        
+
         contourPlot=ax.contourf(Xi,Zeta,speed[:,:,i],levels,vmin=speedMin,
                              vmax=speedMax, cmap='Reds')
 
@@ -418,7 +509,7 @@ def animate_v(ds):
     xi = ds['xi'].values
     zeta = ds['zeta'].values
     tau = ds['tau'].values
-    
+
     # Plot
     plt.rc('text', usetex=False)
 
@@ -446,8 +537,8 @@ def animate_v(ds):
                              vmax=vMax, cmap='RdBu_r')
 
     skip=int(np.floor(np.size(xi)/16))
-    
-    speed=(u**2+w**2)**(1/2)    
+
+    speed=(u**2+w**2)**(1/2)
     speedMax = np.ceil(np.max(speed)*10)/10
 
     uQ=np.sign(u)*(np.abs(u)/speedMax)**(2/3)*speedMax
@@ -475,7 +566,7 @@ def animate_v(ds):
         # Update the line and the axes (with a new xlabel). Return a tuple of
         # "artists" that have to be redrawn for this frame.
         ax.collections = []
-        
+
         contourPlot=ax.contourf(Xi,Zeta,v[:,:,i],levels,vmin=-vMax,
                              vmax=vMax, cmap='RdBu_r')
 
@@ -527,10 +618,10 @@ def animateTheta(ds):
     fig, ax = plt.subplots()
 
     [Xi,Zeta]=np.meshgrid(xi,zeta,indexing='ij')
-        
+
     thetaMax=np.ceil(np.max(theta)/20)*20
     thetaMin=np.floor(np.min(theta)/20)*20
-    
+
     if thetaMax-thetaMin > 200:
         thetaInc = 40
     elif thetaMax-thetaMin > 100:
@@ -539,23 +630,23 @@ def animateTheta(ds):
         thetaInc = 10
 
     levels=np.arange(thetaMin,thetaMax+thetaInc,thetaInc)
-    
+
     contourPlot = ax.contourf(
         Xi, Zeta, theta[:,:,0], cmap='Reds',
-        levels=levels, 
+        levels=levels,
         )
     cbar=plt.colorbar(contourPlot)
     cbar.set_label('Potential Temperature [K]')
-    
+
     ax.contour(
             Xi, Zeta, theta[:,:,0], colors='black',
             linestyles=None, levels=levels,
             linewidths=1.4
             )
-    
+
     skip=int(np.floor(np.size(xi)/16))
-    
-    speed=(u**2+w**2)**(1/2)    
+
+    speed=(u**2+w**2)**(1/2)
     speedMax = np.ceil(np.max(speed)*10)/10
 
     uQ=np.sign(u)*(np.abs(u)/speedMax)**(2/3)*speedMax
@@ -570,7 +661,7 @@ def animateTheta(ds):
     ax.quiver(Xi[sQ::skip,sQ::skip], Zeta[sQ::skip,sQ::skip],
                uQ[sQ::skip,sQ::skip,0], wQ[sQ::skip,sQ::skip,0],
                units='xy', scale=speedMaxQ/(skip*dxi))
-    
+
     plt.title('Potential Temperature [K]')
     plt.xlabel('Distance [' + ds.xi.attrs['units'] + ']')
     plt.ylabel('Height [' + ds.zeta.attrs['units'] + ']')
@@ -583,12 +674,12 @@ def animateTheta(ds):
         global contourPlot
 
         ax.collections = []
-        
+
         contourPlot = ax.contourf(
             Xi, Zeta, theta[:,:,i], cmap='Reds',
-            levels=levels, 
+            levels=levels,
             )
-        
+
         ax.contour(
             Xi, Zeta, theta[:,:,i], colors='black',
             linestyles=None, levels=levels,
